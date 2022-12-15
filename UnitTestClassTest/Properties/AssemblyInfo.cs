14458b552b23b6add8122f9c5d52c3c3ff9d41e6
aza1rat <azazartnykash@gmail.com>
2022-12-15 10:51:05 +0300

    Добавьте файлы проекта.

---
+++ UnitTestClassTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestClassTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestClassTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8dbddce-edd4-4be1-b67c-a24a82a7590c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestClassTest/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ClassTest;
+
+namespace UnitTestClassTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            int a = 3;
+            int b = 4;
+            int expected = 7;
+            //Act
+            ClassNew classNew = new ClassNew();
+            int actual = classNew.Solve(a, b);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
